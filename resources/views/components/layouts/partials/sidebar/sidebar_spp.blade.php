6c42860efe2e0fccba56548fd938e09fe8ece81b
farizDafansyah <121002652+farizDafansyah@users.noreply.github.com>
2024-10-26 15:49:00 +0700

    Revert "fix:bug (#12)"
    This reverts commit db2bf6d0605dc14c5ce61f4197f0499087f63373.

--- resources/views/components/layouts/partials/sidebar/sidebar_spp.blade.php
+++ resources/views/components/layouts/partials/sidebar/sidebar_spp.blade.php
@@ -2,7 +2,7 @@
     <li class="sidebar-title">Menu</li>
 
     <li class="sidebar-item {{ Request::routeIs('spp.dashboard') ? 'active' : '' }}">
-        <a href="{{ route('spp.dashboard') }}" class='sidebar-link'>
+        <a href="{{ route('spp.dashboard') }}" wire:navigate class='sidebar-link'>
             <i class="bi bi-grid-fill"></i>
             <span>Dashboard</span>
         </a>
@@ -11,25 +11,25 @@
     <li class="sidebar-title">Pembayaran</li>
 
     <li class="sidebar-item {{ Request::routeIs('spp.pembayaran') ? 'active' : '' }}">
-        <a href="{{ route('spp.pembayaran') }}" class='sidebar-link'>
+        <a href="{{ route('spp.pembayaran') }}" wire:navigate class='sidebar-link'>
             <i class="bi bi-cash-coin"></i>
             <span>Pembayaran SPP Santri</span>
         </a>
     </li>
     <li class="sidebar-item {{ Request::routeIs('spp.list-item-pembayaran') ? 'active' : '' }}">
-        <a href="{{ route('spp.list-item-pembayaran') }}" class='sidebar-link'>
+        <a href="{{ route('spp.list-item-pembayaran') }}" wire:navigate class='sidebar-link'>
             <i class="bi bi-list-check"></i>
             <span>List Item Pembayaran</span>
         </a>
     </li>
     <li class="sidebar-item {{ Request::routeIs('spp.pembayaran-cicilan') ? 'active' : '' }}">
-        <a href="{{ route('spp.pembayaran-cicilan') }}" class='sidebar-link'>
+        <a href="{{ route('spp.pembayaran-cicilan') }}" wire:navigate class='sidebar-link'>
             <i class="bi bi-cash-stack"></i>
             <span>Pembayaran Cicilan</span>
         </a>
     </li>
     <li class="sidebar-item {{ Request::routeIs('spp.tambah-santri') ? 'active' : '' }}">
-        <a href="{{ route('spp.tambah-santri') }}" class='sidebar-link'>
+        <a href="{{ route('spp.tambah-santri') }}" wire:navigate class='sidebar-link'>
             <i class="bi bi-cash-stack"></i>
             <span>Tambah Santri</span>
         </a>
@@ -38,7 +38,7 @@
     <li class="sidebar-title">Laporan Keuangan</li>
 
     <li class="sidebar-item {{ Request::routeIs('spp.laporan-keuangan') ? 'active' : '' }}">
-        <a href="{{ route('spp.laporan-keuangan') }}" class='sidebar-link'>
+        <a href="{{ route('spp.laporan-keuangan') }}" wire:navigate class='sidebar-link'>
             <i class="bi bi-bar-chart-fill"></i>
             <span>Laporan keuangan SPP</span>
         </a>
